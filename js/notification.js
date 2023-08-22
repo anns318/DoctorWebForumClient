e215340c4e027cc23d1f2d399c3405fb3f64c22a
anns318 <an310898@gmai.com>
2023-08-23 00:31:37 +0700

    add notification function

--- js/notification.js
+++ js/notification.js
@@ -7,7 +7,6 @@ connectionNotification
   .start()
   .then(function () {
     console.log("connected!");
-    SendNotification(1);
   })
   .catch(function (err) {
     return console.error(err.toString());
@@ -16,42 +15,97 @@ connectionNotification
 connectionNotification.on(
   "HaveNewNotification",
   function (postId, notification) {
-    let div = document.createElement("div");
-    div.className = "notification";
-    div.innerHTML = notification;
-    document.getElementById("container-notify").appendChild(div);
-    $("#container-notify").fadeIn(200);
-    setTimeout(() => {
-      $("#container-notify").fadeOut(1000, () => {
-        document.getElementById("container-notify").innerHTML = "";
-      });
-    }, 2000);
+    let a = document.createElement("a");
+    a.className = "container-notify";
+    a.href = `/post.html?id=${postId}`;
+    a.innerHTML = `<div class="notification">${notification}</div>`;
+    document.getElementById("container-notify-display").appendChild(a);
+    $("#container-notify-display").fadeIn(200);
+    // setTimeout(() => {
+    //   $("#container-notify-display").fadeOut(1500, () => {
+    //     document.getElementById("container-notify-display").innerHTML = "";
+    //   });
+    // }, 2000);
   }
 );
 
 function SendNotification(PostId) {
-  const htmlNotification = `
-                 <img src="${
-                   userData.avatar == "null"
-                     ? "https://localhost:7157/images/users/0.png"
-                     : "https://localhost:7157/" + userData.avatar
-                 }" alt="User 1">
-                    <div class="notification-content">
-                        <h3>${userData.firstName} ${
-    userData.lastName
-  } comment on your post</h3>
-                        <p id="notificationTime">Just Now</p>
-                    </div>
-            `;
+  console.log(PostId);
+  const userAvatar =
+    userData.avatar == "null"
+      ? "https://localhost:7157/images/users/0.png"
+      : "https://localhost:7157/" + userData.avatar;
+  const userName = `${userData.firstName} ${userData.lastName}`;
 
   connectionNotification
     .invoke(
       "SendNotification",
       PostId.toString(),
-      htmlNotification,
+      userData.userId.toString(),
+      userName,
+      userAvatar,
+      "comment on your post",
       Date.now().toString()
     )
     .catch(function (err) {
       return console.error(err.toString());
     });
+}
+
+document
+  .getElementById("comment-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const userId = userData.userId;
+    const comment = document.getElementById("comment-text").value;
+
+    if (userId && comment) {
+      const newComment = document.createElement("div");
+      newComment.classList.add("comment");
+      newComment.innerHTML = `
+         <div class="comment">
+            <img class="user-avatar" src="${
+              userData.userAvatar
+                ? userData.userAvatar
+                : "https://localhost:7157/images/users/0.png"
+            }" alt="">
+            <div class="comment-content">
+                <p class="comment-user">${userData.firstName} ${
+        userData.lastName
+      }</p>
+                <p class="comment-text">${comment}</p>
+            </div>
+        </div>
+        `;
+
+      const formData = { postId, userId, comment };
+
+      const res = await PostAPI(
+        "https://localhost:7157/api/Comments",
+        formData
+      );
+
+      if (res.status === 201) {
+        document.querySelector(".comment-container").appendChild(newComment);
+        document.getElementById("comment-text").value = "";
+
+        const countCommentPost = document.querySelectorAll(".countCommentPost");
+
+        countCommentPost.forEach((element) => {
+          if (+element.getAttribute("countCommentPost") === postId) {
+            element.textContent = +element.textContent + 1;
+          }
+        });
+        SendNotification(postId);
+      } else {
+        alert("Something go wrong, please try again!");
+      }
+    }
+  });
+
+let notificationArr = [];
+async function getNotificationByUser() {
+  const userId = userData.userId;
+  const data = await GetAPI(``);
 }
